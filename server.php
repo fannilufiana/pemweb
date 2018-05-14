0ab8bf9841746eb0071656c25f8172446bc68e48
fannilufiana <37866746+fannilufiana@users.noreply.github.com>
2018-05-14 13:27:47 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,73 @@
+<?php
+	session_start();
+	
+	$username = "";
+	$email = "";
+	$errors = array();
+	
+	//connect to database
+	$db = mysqli_connect('localhost', 'root', '', 'registration');
+	
+	//if register button is clicked
+	if(isset($_POST['register'])) {
+		$username = mysql_real_escape_string($_POST['username']);
+		$email = mysql_real_escape_string($_POST['email']);
+		$password = mysql_real_escape_string($_POST['password']);
+		
+		//ensure that form fields are filled properly
+		if(empty($username)) {
+			array_push($errors, "Username is required");
+		}
+		if(empty($email)) {
+			array_push($errors, "Email is required");
+		}
+		if(empty($password)) {
+			array_push($errors, "Password is required");
+		}
+		
+		//if there are no errors, save user to database
+		if(count($errors) == 0) {
+			$password = md5($password);
+			$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+			mysqli_query($db, $sql);
+			$_SESSION['username'] = $username;
+			$_SESSION['success'] = "You are now logged in";
+			header('location: index.php'); //redirect to home page
+		}
+	}
+	
+	//log user in from login page
+	if (isset($_POST['login'])) {
+		$username = mysql_real_escape_string($_POST['username']);
+		$password = mysql_real_escape_string($_POST['password']); 
+		
+		//ensure that form fields are filled properly
+		if(empty($username)) {
+			array_push($errors, "Username is required");
+		}
+		if(empty($password)) {
+			array_push($errors, "Password is required");
+		}
+		if (count($errors) == 0) {
+			$password = md5($password); //encrypt password before comparing with that from database
+			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+			$result = mysqli_query($db, $query);
+			if (mysqli_num_rows($result) == 1) {
+				//log user in
+				$_SESSION['username'] = $username;
+				$_SESSION['success'] = "You are now logged in";
+				header('location: index.php'); //redirect to home page
+			}else {
+				array_push($errors, "wrong username/password combination");
+				header('location: login.php');
+			}
+		}
+	}
+	
+	//logout
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header('location: login.php');
+	}
+?>
